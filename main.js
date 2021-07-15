b49fdf226a587326898ade895936852fe5ff7b29
NJ-2009 <74959872+NJ-2009@users.noreply.github.com>
2021-07-15 16:50:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+noseX=0
+noseY=0
+function preload(){
+    spectacles=loadImage('');
+}
+
+function setup(){
+    canvas= createCanvas(300,300);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x-16;
+        noseY=results[0].pose.nose.y-16;
+        console.log("nose x ="+ noseX);
+        console.log("nose y ="+ noseY);
+    }
+
+}
+function modelLoaded(){
+    console.log('Posenet is on');
+}
+function draw(){
+    image(video,0,0,300,300);
+    image(spectacles,noseX,noseY,40,40);
+}
+function take_pic(){
+    save("filter-spectacles.png");
+}
